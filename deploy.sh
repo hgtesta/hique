4fb98a8878f0cfb46d06c2553216172fa463603a
Henrique G. Testa <hgtesta@gmail.com>
2016-03-11 12:11:46 -0300

    Changed photos

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/local/bin/fish
+
+# ssh git@amize.com.br 'bash -s' < local_script.sh
+
+echo "Uploading site to hique-new"
+if scp -r _site git@amize.com.br:/webapps/hique-new
+
+  echo "Delete hique-old"
+  ssh git@amize.com.br "rm -rf /webapps/hique-old"
+
+  echo "Rename hique to hique-old"
+  ssh git@amize.com.br "mv /webapps/hique /webapps/hique-old"
+
+  echo "Rename hique-new to hique"
+  ssh git@amize.com.br "mv /webapps/hique-new /webapps/hique"
+
+end
